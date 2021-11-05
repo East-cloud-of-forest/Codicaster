323417fd7322c47527c75aefa79b638a194e649f
dongun lim <ecof1911@gmail.com>
2021-11-05 23:25:41 +0900

    날씨 API에서 받아온 데이터로 기본 화면 구축
    getElementalBy.. 를 중복사용 하면 더 자세한 태그를 참조 가능.
    new Image 와 .src 를 이용하여 함수에 이미지를 저장해두었다가 appendChild 로 삽입이 가능.
    fetch를 연달아 두번 가능하나 이전에서 정의된 값은 사용하지 못하므로 안에 겹쳐서 사용하거나
    다른 방법을 강구해야 함.
    getTiem 인자를 new Date() 의 괄호 안에 넣어서 변형 할 수 있음.
    
    현재날씨API를 사용하다가 시간별 데이터가 필요하여 새로운 API를 추가했는데, 이 API에서 현재
    데이터까지 출력해주지만 받아야 하는 도시 파라미터가 위도, 경도 라서 임시방편으로 현재날씨
    API에서 주는 위도 경도를 이용하여 사용할 계획, 추후 현재날씨 API를 없애면서 도시별 위도
    경도를 받아올 방법이 필요함.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,20 @@ class App {
     this.W.style.height = '500px'
     this.W.style.background = 'pink'
 
+    this.weatherIcon = document.getElementById('weatherIcon')
+    this.icon = new Image()
+
+    this.temp = document.getElementById('temp').getElementsByTagName('h1')[0]
+    this.tempIcon = document.getElementById('temp').getElementsByTagName('i')[0]
+    this.tempIcon.style.display = 'none'
+
+    this.lon = ''
+    this.lat = ''
+
+    this.location = document
+      .getElementById('location')
+      .getElementsByTagName('p')[0]
+
     this.city = document.getElementById('city')
     this.city.addEventListener('change', this.clickCity.bind(this))
     this.clickCity()
@@ -23,6 +37,9 @@ class App {
           console.log(data)
 
           let today = new Date()
+          let bbb = 1636167600
+          let aaa = new Date(bbb)
+          console.log(aaa)
           let year = today.getFullYear()
           let month = today.getMonth()
           let date = today.getDate()
@@ -44,16 +61,37 @@ class App {
           }
           let time = today.getHours()
 
-          this.W.innerText = `${data.name}
+          this.icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+          this.weatherIcon.appendChild(this.icon)
+
+          this.temp.innerHTML = `${data.main.temp}`
+          this.tempIcon.style.display = 'block'
+
+          this.location.innerText = `${
+            this.city.options[this.city.options.selectedIndex].text
+          }`
+
+          this.lon = data.coord.lon
+          this.lat = data.coord.lat
+
+          this.W.innerText = ''
+          this.W.innerText += `${data.name}
           온도 : ${data.main.temp}
           최고온도 : ${data.main.temp_max}
           최저온도 : ${data.main.temp_min}
-          
-          ${icon}
 
           ${year}년 ${month + 1}월 ${date}일 ${day}
           ${time}시
           `
+          fetch(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${this.lat}&lon=${this.lon}&exclude=current,minutely,hourly,alerts&appid=b905f0c03119f5162e6063c34f4e9e05`,
+          )
+            .then((response) => response.json())
+            .then((data) => {
+              console.log(data)
+              console.log(this.lon)
+              console.log(this.lat)
+            })
         })
         .catch((error) => console.log(('error', error)))
     }
