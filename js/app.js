b346de6a83fa5ec77ef1f044b9d16c78e08e6630
dongun lim <ecof1911@gmail.com>
2021-11-12 23:24:40 +0900

    서브페이지 빌드, 48시간 시간별 온도 및 날씨, 일주일간의 날씨 빌드

--- js/app.js
+++ js/app.js
@@ -2,11 +2,13 @@ class App {
   constructor() {
     this.W = document.createElement('div')
     document.body.appendChild(this.W)
-    this.W.style.width = '100%'
-    this.W.style.maxWidth = '1000px'
-    this.W.style.height = '500px'
+    this.W.style.width = '300px'
+    this.W.style.height = '300px'
     this.W.style.background = 'pink'
     this.W.style.margin = 'auto'
+    this.W.style.position = 'absolute'
+    this.W.style.top = '50px'
+    this.W.style.left = '50px'
 
     this.weatherIcon = document.getElementById('weatherIcon')
     this.icon = new Image()
@@ -28,6 +30,31 @@ class App {
     this.loaction = document.getElementsByTagName('location')
     this.location.addEventListener('click', this.gps.bind(this))
     this.gps()
+
+    // 서브페이지
+    // 서브페이지내의 시간별 온도 버튼
+    this.hourTempNext = document.getElementById('hourTempNext')
+    this.hourTemp = document.getElementById('hourTemp')
+    let a = 0
+    this.hourTempNext.addEventListener('click', () => {
+      a += 376.3
+      this.hourTemp.style.right = `${a}px`
+    })
+
+    // 서브페이지 토글
+    this.test = document.getElementById('subPage')
+    this.testBtn = document
+      .getElementById('subPage')
+      .getElementsByTagName('button')[0]
+    this.btn = document.getElementById('timeTemp')
+    this.btn.addEventListener('click', () => {
+      this.test.style.display = 'flex'
+    })
+    this.testBtn.addEventListener('click', () => {
+      this.test.style.display = 'none'
+      a = 0
+      this.hourTemp.style.right = `${a}px`
+    })
   }
 
   clickCity() {
@@ -51,22 +78,7 @@ class App {
         let year = today.getFullYear()
         let month = today.getMonth()
         let date = today.getDate()
-        let day = today.getDay()
-        if (day == 0) {
-          day = '일요일'
-        } else if (day == 1) {
-          day = '월요일'
-        } else if (day == 2) {
-          day = '화요일'
-        } else if (day == 3) {
-          day = '수요일'
-        } else if (day == 4) {
-          day = '목요일'
-        } else if (day == 5) {
-          day = '금요일'
-        } else if (day == 6) {
-          day = '토요일'
-        }
+        let day = this.getDay(today.getDay())
         let time = today.getHours()
 
         this.icon.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
@@ -100,11 +112,12 @@ class App {
 
   timeTemp(lat, lon) {
     fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,alerts&appid=b905f0c03119f5162e6063c34f4e9e05`,
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,alerts&appid=b905f0c03119f5162e6063c34f4e9e05`,
     )
       .then((response) => response.json())
       .then((data) => {
         console.log(data)
+        // 메인페이지 시간별 온도
         this.morn = document.getElementsByClassName('morn')[0]
         this.eve = document.getElementsByClassName('eve')[0]
         this.day = document.getElementsByClassName('day')[0]
@@ -114,7 +127,88 @@ class App {
         this.eve.innerText = Math.round(data.daily[0].temp.eve)
         this.day.innerText = Math.round(data.daily[0].temp.day)
         this.night.innerText = Math.round(data.daily[0].temp.night)
+
+        // 시간별 온도 서브 페이지, 시간별 온도
+        this.hourTemp = document.getElementById('hourTemp')
+        this.hourTemp.innerHTML = ``
+
+        for (let i = 0; i < data.hourly.length; ++i) {
+          let dt = data.hourly[i].dt * 1000
+          let time = new Date(dt)
+          let hour = `<p>${time.getHours()}</p>`
+          let temp = Math.round(data.hourly[i].temp)
+
+          if (time.getHours() == 0) {
+            if (time.getDate() == new Date().getDate()) {
+              hour = `<p class="box">오늘</P>`
+            }
+            if (time.getDate() == new Date().getDate() + 1) {
+              hour = `<p class="box">내일</P>`
+            }
+            if (time.getDate() == new Date().getDate() + 2) {
+              hour = `<p class="box">모레</P>`
+            }
+          }
+
+          this.hourTemp.innerHTML += `<div>
+            <p>${temp}˚</p>
+            <img src="http://openweathermap.org/img/wn/${data.hourly[i].weather[0].icon}@2x.png" alt="icon ${i}">
+            ${hour}
+          </div>`
+        }
+
+        this.weekTemp = document.getElementById('weekTemp')
+        this.weekTemp.innerHTML = ''
+
+        for (let i = 0; i < data.daily.length; ++i) {
+          let dt = data.daily[i].dt * 1000
+          let today = new Date(dt)
+          let day = this.getDay(today.getDay())
+          let date = `${today.getMonth() + 1}.${today.getDate()}`
+          if (today.getDate() == new Date().getDate()) {
+            day = '오늘'
+          }
+          if (today.getDate() == new Date().getDate() + 1) {
+            day = '내일'
+          }
+
+          let maxtemp = Math.round(data.daily[i].temp.max)
+          let mintemp = Math.round(data.daily[i].temp.min)
+
+          this.weekTemp.innerHTML += `
+          <article>
+            <div>
+              <p>${day}</p>
+              <p>${date}</p>
+            </div>
+            <div>
+              <p>${maxtemp}</p>
+              <p>${mintemp}</p>
+            </div>
+          </article>
+          `
+        }
       })
+  }
+
+  //요일
+  getDay(a) {
+    if (a == 0) {
+      a = '일요일'
+    } else if (a == 1) {
+      a = '월요일'
+    } else if (a == 2) {
+      a = '화요일'
+    } else if (a == 3) {
+      a = '수요일'
+    } else if (a == 4) {
+      a = '목요일'
+    } else if (a == 5) {
+      a = '금요일'
+    } else if (a == 6) {
+      a = '토요일'
+    }
+    return a
   }
 
   // gps API
@@ -141,7 +235,8 @@ class App {
   }
 
   showErrorMsg() {
-    this.location.innerText = '위치 정보를 가져올 수 없습니다.'
+    this.location.innerText =
+      '위치 정보를 가져올 수 없습니다. 지역을 선택해주세요.'
   }
 }
 
