07ec8acf60bb7302b5dab0b73dc40b3ffaeeff61
dongun lim <ecof1911@gmail.com>
2022-03-07 22:17:36 +0900

    옷 이미지 추가

--- js/subPageCloth.js
+++ js/subPageCloth.js
@@ -1,3 +1,5 @@
+import ClothSVG from "./clothSVG.js"
+
 export class SubPageCloth {
   constructor() {
     this.clothes = document.getElementById('clothes')
@@ -6,15 +8,13 @@ export class SubPageCloth {
   mainPageHtml(data) {
     let curtemp = Math.round(data.main.temp)
 
-    // curtemp =28
-
     let clothImg = []
     let clothTag = []
     let clothText = ``
     let boxColor = ``
 
     if(curtemp >= 28) {
-      clothTag = ["민소매", "반팔", "짧은 하의"]
+      clothTag = ["민소매", "반팔", "반바지"]
       clothText = `너무 더워요. 얇게 입어 열사병에 주의하고 실내에 있기로 해요.`
       boxColor = `#e43d2b`
     } else if (23 <= curtemp && curtemp <= 27) {
@@ -30,7 +30,7 @@ export class SubPageCloth {
       clothText = `선선하고 좋은 날씨예요. 밤에는 추울 수도 있으니 외투를 챙기도록 해요.`
       boxColor = `#cfac2d`
     } else if (12 <= curtemp && curtemp <= 16) {
-      clothTag = ["자켓", "가디건", "청자켓", "니트", "청바지"]
+      clothTag = ["자켓", "가디건", "니트", "청바지"]
       clothText = `조금 쌀쌀할 수 있어요. 멋도 좋지만 따스한 외투를 하나씩 챙겨요.`
       boxColor = `#b7ae32`
     } else if (9 <= curtemp && curtemp <= 11) {
@@ -42,15 +42,14 @@ export class SubPageCloth {
       clothText = `조금 추운 날씨예요. 내의를 챙겨 입고 따뜻하게 다녀요.`
       boxColor = `#4998b6`
     } else if (4 >= curtemp) {
-      clothTag = ["패딩", "두꺼운 코트", "기모", "목도리"]
+      clothTag = ["패딩", "두꺼운 코트", "기모", "목도리", "장갑"]
       clothText = `날씨가 아주 추워요. 꽁꽁 싸매고 둘러야 해요. 두툼하게 입도록 해요.`
       boxColor = `#2d82c3`
     }
 
     for (let i in clothTag) {
+      clothImg[i] = `<div class="clothImges" value="${i}">${ClothSVG[clothTag[i]]}</div>`
       clothTag[i] = `<span value="${i}"># ${clothTag[i]}</span>`
-      
-      clothImg[i] = `<div class="clothImges" value="${i}">${clothTag[i]}</div>`
     }
 
     this.clothes.innerHTML = `
